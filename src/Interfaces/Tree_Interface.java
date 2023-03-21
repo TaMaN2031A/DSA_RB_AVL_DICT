77f918f18209a3b2c21c6f086fc89bc3abf82cde
Sarah SmithAbdullah <tamanabdullah9@gmail.com>
2023-03-21 21:30:05 +0200

    PreFinal

---
+++ src/Interfaces/Tree_Interface.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+import java.io.IOException;
+
+public interface Tree_Interface<T> {
+    int insert(T node) throws IOException;
+    int delete(T node) throws IOException;
+    boolean search(T node);
+    int TreeSize();
+    int TreeHeight();
+    void ends() throws IOException;
+}
